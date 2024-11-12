7edc6e0cf06370ac02afc78ee1699a421e7d9e6f
Doug Holland <doug.holland@outlook.com>
2024-11-12 10:02:56 -0800

    Initial Commit

---
+++ ColorTest/ColorTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorTestApp.swift
+//  ColorTest
+//
+//  Created by Doug Holland on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
